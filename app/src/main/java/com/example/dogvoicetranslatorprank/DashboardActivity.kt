7291f6e307cd181fd7f6a2adbfb48ef66046d224
AmitKuveskar <Kuveskar.amit@gmai.com>
2024-05-31 11:18:13 +0530

    Kotlin

--- app/src/main/java/com/example/dogvoicetranslatorprank/DashboardActivity.kt
+++ app/src/main/java/com/example/dogvoicetranslatorprank/DashboardActivity.kt
@@ -1,12 +1,17 @@
 package com.example.dogvoicetranslatorprank
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class DashboardActivity : AppCompatActivity() {
+    lateinit var ButtonDrawer: ImageButton
+    lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,12 @@ class DashboardActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        drawerLayout = findViewById(R.id.drawerLayout)
+        ButtonDrawer = findViewById(R.id.ButtonDrawer)
+
+        ButtonDrawer.setOnClickListener {
+            drawerLayout.open()
         }
     }
 }
